5626e0e55a04f32e1d599cc580cf7200e5c1c672
sogawa-yk <daybreaks.link@gmail.com>
2024-07-12 12:05:35 +0900

    fix

--- openshift/infrastructure.tf
+++ openshift/infrastructure.tf
@@ -730,11 +730,11 @@ resource "oci_identity_policy" "openshift_control_plane_nodes" {
   description    = "OpenShift control_plane nodes instance principal"
   name           = "${var.cluster_name}_control_plane_nodes"
   statements = [
-    "Allow dynamic-group ${oci_identity_dynamic_groups.openshift_control_plane_nodes.name} to manage volume-family in compartment id ${var.compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_groups.openshift_control_plane_nodes.name} to manage instance-family in compartment id ${var.compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_groups.openshift_control_plane_nodes.name} to manage security-lists in compartment id ${var.compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_groups.openshift_control_plane_nodes.name} to use virtual-network-family in compartment id ${var.compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_groups.openshift_control_plane_nodes.name} to manage load-balancers in compartment id ${var.compartment_ocid}",
+    "Allow dynamic-group ${data.oci_identity_dynamic_groups.openshift_control_plane_nodes.name} to manage volume-family in compartment id ${var.compartment_ocid}",
+    "Allow dynamic-group ${data.oci_identity_dynamic_groups.openshift_control_plane_nodes.name} to manage instance-family in compartment id ${var.compartment_ocid}",
+    "Allow dynamic-group ${data.oci_identity_dynamic_groups.openshift_control_plane_nodes.name} to manage security-lists in compartment id ${var.compartment_ocid}",
+    "Allow dynamic-group ${data.oci_identity_dynamic_groups.openshift_control_plane_nodes.name} to use virtual-network-family in compartment id ${var.compartment_ocid}",
+    "Allow dynamic-group ${data.oci_identity_dynamic_groups.openshift_control_plane_nodes.name} to manage load-balancers in compartment id ${var.compartment_ocid}",
   ]
   provider = oci.home
 }
