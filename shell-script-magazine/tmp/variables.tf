f25b70b82fee9fe0f56380e228166a77553fde96
sogawa-yk <daybreaks.link@gmail.com>
2024-10-22 11:30:58 +0900

    first commit

---
+++ shell-script-magazine/tmp/variables.tf
@@ -0,0 +1,3 @@
+variable compartment_ocid { default = "ocid1.compartment.oc1..aaaaaaaar4axbrfulk7ixqygr4udyhsb3dsnw7yz627h3lhx55klkgzoehna" }
+variable region { default = "ap-tokyo-1" }
+variable email { default = "user@example.com"}
